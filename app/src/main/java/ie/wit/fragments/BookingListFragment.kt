11124e4e844a1eedc8fd79cdd6e3b90ecbab3d31
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-04-15 17:36:47 +0100

    Completed section 4 of Networking Lab

--- app/src/main/java/ie/wit/fragments/BookingListFragment.kt
+++ app/src/main/java/ie/wit/fragments/BookingListFragment.kt
@@ -1,23 +1,34 @@
 package ie.wit.fragments
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import ie.wit.R
 /*
 import ie.wit.adapters.ValetListener
 */
 import ie.wit.adapters.ValetingAdapter
 import ie.wit.main.ValetApp
+import ie.wit.models.ValetModel
+import ie.wit.utils.*
 import kotlinx.android.synthetic.main.fragment_booking_list.view.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class BookingListFragment : Fragment()/*, ValetListener*/ {
+class BookingListFragment : Fragment(), AnkoLogger, Callback<List<ValetModel>> {
 
     lateinit var app: ValetApp
+    lateinit var loader : AlertDialog
+    lateinit var root: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +40,12 @@ class BookingListFragment : Fragment()/*, ValetListener*/ {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var root = inflater.inflate(R.layout.fragment_booking_list, container, false)
+        root = inflater.inflate(R.layout.fragment_booking_list, container, false)
 
         root.recyclerView.layoutManager = LinearLayoutManager(activity)
         root.recyclerView.adapter = ValetingAdapter(app.valets)
+        loader = createLoader(activity!!)
+        setSwipeRefresh()
 
         //loadBookings()
 
@@ -46,5 +59,48 @@ class BookingListFragment : Fragment()/*, ValetListener*/ {
             BookingListFragment().apply {
                 arguments = Bundle().apply { }
             }
+    }
+
+    fun setSwipeRefresh() {
+        root.swiperefresh.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
+            override fun onRefresh() {
+                root.swiperefresh.isRefreshing = true
+                getAllBookings()
+            }
+        })
+    }
+
+    fun checkSwipeRefresh() {
+        if (root.swiperefresh.isRefreshing) root.swiperefresh.isRefreshing = false
+    }
+
+    override fun onFailure(call: Call<List<ValetModel>>, t: Throwable) {
+        info("Retrofit Error : $t.message")
+        serviceUnavailableMessage(activity!!)
+        checkSwipeRefresh()
+        hideLoader(loader)
+    }
+
+    override fun onResponse(call: Call<List<ValetModel>>,
+                            response: Response<List<ValetModel>>
+    ) {
+        serviceAvailableMessage(activity!!)
+        info("Retrofit JSON = ${response.body()}")
+        app.valets = response.body() as ArrayList<ValetModel>
+        root.recyclerView.adapter = ValetingAdapter(app.valets)
+        root.recyclerView.adapter?.notifyDataSetChanged()
+        checkSwipeRefresh()
+        hideLoader(loader)
+    }
+
+    fun getAllBookings() {
+        showLoader(loader, "Downloading the Donations List")
+        var callGetAll = app.valetService.getall()
+        callGetAll.enqueue(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getAllBookings()
     }
 }
