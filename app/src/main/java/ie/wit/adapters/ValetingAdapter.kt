cf9d9f32de682825f0b5c5f69754ab2c9c7aa7af
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-05-03 13:32:40 +0100

    Final Release of the app

--- app/src/main/java/ie/wit/adapters/ValetingAdapter.kt
+++ app/src/main/java/ie/wit/adapters/ValetingAdapter.kt
@@ -19,58 +19,6 @@ interface ValetingListener{
     fun onValetClick(valet: ValetModel)
 }
 
-/*class ValetingAdapter constructor(var valets: ArrayList<ValetModel>,
-                                  private val listener: ValetingListener, listall : Boolean)
-    : RecyclerView.Adapter<ValetingAdapter.MainHolder>() {
-
-    val listAll = listall
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        return MainHolder(
-            LayoutInflater.from(parent?.context).inflate(
-                R.layout.card_valet,
-                parent,
-                false
-            )
-        )
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val valet = valets[holder.adapterPosition]
-        holder.bind(valet, listener, listAll)
-    }
-
-    override fun getItemCount(): Int = valets.size
-
-    fun removeAt(position: Int){
-        valets.removeAt(position)
-        notifyItemRemoved(position)
-    }
-
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bind(valet: ValetModel, listener: ValetingListener, listAll: Boolean) {
-            itemView.tag = valet
-            itemView.carBrandDisplay.text = valet.brand
-            itemView.carModelDisplay.text = valet.model
-            itemView.licensePlateDisplay.text = valet.numberPlate
-            itemView.dateShown.text = valet.date
-            if(valet.isfavourite) itemView.imagefavourite.setImageResource(android.R.drawable.star_big_on)
-
-            if(!listAll)
-                itemView.setOnClickListener { listener.onValetClick(valet) }
-
-            if(!valet.profilepic.isEmpty()){
-                Picasso.get().load(valet.profilepic.toUri())
-                    //.resize(180, 180)
-                    .transform(CropCircleTransformation())
-                    .into(itemView.imageIcon)
-            }
-            else
-                itemView.imageIcon.setImageResource(R.mipmap.ic_launcher_darth_maul_foreground)
-        }
-    }
-}*/
 class ValetingAdapter(options: FirebaseRecyclerOptions<ValetModel>,
                       private val listener: ValetingListener?)
     : FirebaseRecyclerAdapter<ValetModel,
